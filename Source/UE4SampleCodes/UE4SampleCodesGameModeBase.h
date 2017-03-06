77d1fcc5e94fd6fbf00d1de886197c99c64f7710
Joseph Choi <jc566@njit.edu>
2017-03-06 03:16:06 -0500

    Initial Commit + Floating Actor Script

---
+++ Source/UE4SampleCodes/UE4SampleCodesGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "UE4SampleCodesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4SAMPLECODES_API AUE4SampleCodesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
